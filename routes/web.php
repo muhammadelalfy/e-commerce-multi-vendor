8fb7465b1acfa3d1280f73d8647b6faf71301ddd
muhammadelalfy <dev.muhamadelalfy@gmail.com>
2022-08-23 18:02:49 +0200

    start

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+$route = app()->make('router');
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+$route->get('/', function () {
     return view('welcome');
 });
