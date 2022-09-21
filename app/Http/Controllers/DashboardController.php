ccbdadf33b3ea3ac26ea8e0a2333cd627fb5bc0a
muhammadelalfy <dev.muhamadelalfy@gmail.com>
2022-09-21 17:59:01 +0200

    blade step1

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -3,10 +3,17 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class DashboardController extends Controller
 {
+    #responce view , redirect , file , json
     public function index(){
-        return view('dashboard');
+//        $view = app()->make('view');
+//        return $view->make('dashboard');
+
+//        return View::make('dashboard');
+         return response()->view('dashboard.index');
+         
     }
 }
