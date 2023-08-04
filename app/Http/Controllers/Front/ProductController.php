fe74f9eac40fe1de8a485cebc730a403c2808260
MuhammadElalfy <48968301+muhammadelalfy@users.noreply.github.com>
2023-08-04 22:15:36 +0300

    feat product details page

--- app/Http/Controllers/Front/ProductController.php
+++ app/Http/Controllers/Front/ProductController.php
@@ -2,17 +2,21 @@
 
 namespace App\Http\Controllers\Front;
 
+use App\Enums\ProductStatusEnum;
 use App\Http\Controllers\Controller;
 use App\Models\Product;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    public function index(){
+    public function index()
+    {
     }
 
-    public function show(Product $product){
-        dd($product);
-        // abort(404);
+    public function show(Product $product)
+    {
+        if (!$product->status == ProductStatusEnum::Active)
+            abort(404);
+        return view('front.products.show',compact('product'));
     }
 }
--- resources/views/components/product.blade.php
+++ resources/views/components/product.blade.php
@@ -3,21 +3,23 @@
     <div class="product-image">
         <img src="{{ $product->image_url }}" alt="#">
         {{-- if has sale --}}
-        @if ($product->sale_percent)
-            <span class="sale-price">-{{ $product->sale_percent }}%</span>
+
+        @if ($product->sale_percent >= 0.0)
+            <span class="sale-tag">-{{ $product->sale_percent }}%</span>
         @endif
         {{-- if is new --}}
         @if ($product->new)
             <span class="sale-price">-{{ $product->new }}%</span>
         @endif
         <div class="button">
-            <a href="{{ route('product.show',$product->slug) }}" class="btn"><i class="lni lni-cart"></i> Add to Cart</a>
+
+            <a href="{{ route('site.products.show',$product->slug) }}" class="btn"><i class="lni lni-cart"></i> Add to Cart</a>
         </div>
     </div>
     <div class="product-info">
         <span class="category">{{ $product->category->name }}</span>
         <h4 class="title">
-            <a href="{{ route('product.show',$product->slug) }}">{{ $product->name }}</a>
+            <a href="{{ route('site.products.show',$product->slug) }}">{{ $product->name }}</a>
         </h4>
         <ul class="review">
             <li><i class="lni lni-star-filled"></i></li>
